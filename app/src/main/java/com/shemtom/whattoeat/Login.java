485e0ab5f0bc5e385b1728505f15c1f771621108
Shem Tom <63319003+shemtomke@users.noreply.github.com>
2023-01-29 20:19:50 +0300

    onBoarding

---
+++ app/src/main/java/com/shemtom/whattoeat/Login.java
@@ -0,0 +1,14 @@
+package com.shemtom.whattoeat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
